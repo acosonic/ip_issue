64405cf1c0cc73350ebefce97bcb65c4e82edc90
Aleksandar Pavić <acosonic@gmail.com>
2024-03-14 08:27:26 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,17 +1,10 @@
 Redmine::Plugin.register :ip_issue do
-  name 'Ip to Issue plugi'
+  name 'Ip to Issue plugin'
   author 'Aleksandar Pavic'
   description 'This plugin for Redmine adds IP address to issue.'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/acosonic/ip_issue'
+  author_url 'https://www.packtpub.com/big-data-and-business-intelligence/redmine-cookbook'
 end
 
 require_dependency 'ip_issue_issue_hook'
-
-
-
-# Path: app/models/issue.rb
-# Compare this snippet from app/models/issue.rb:
-# class Issue < ActiveRecord::Base
-
